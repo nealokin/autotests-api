145b4ef4943d9fedea7c9da9ebe6664928148afd
nealokin <jimse@ya.ru>
2026-01-05 03:13:01 +0700

    Знакомство с WebSocket протоколом. Практика работы с WebSocket протоколом

---
+++ websocket_users_client.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+async def client():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        message = "Привет, сервер!"
+        print(f"Отправка: {message}")
+        await websocket.send(message)
+
+        for _ in range(5):
+            response_message = await websocket.recv()
+            print(response_message)
+
+asyncio.run(client())
